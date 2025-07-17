a044d1240c55e3112aad51dd0135e7a26732ed1e
PanKatofel <pankatofel@gmail.com>
2025-07-17 17:13:34 +0200

    Decode And Encode Morse

---
+++ morse.py
@@ -0,0 +1,61 @@
+letter_to_morse = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    ' ': ' '
+}
+morse_to_letter = {value: key for key, value in letter_to_morse.items()}
+
+class Morse(list):
+    """Class which represents Morse code"""
+    pass
+
+
+def encode(text: str) -> Morse:
+    """Encoding text to Morse code"""
+    text = text.lower()
+    list_text = list(text)
+    morse = Morse()
+
+    # Intentional skip of char which are not in list
+    for letter in list_text:
+        if letter in letter_to_morse:
+            morse.append(letter_to_morse[letter])
+
+    return morse
+
+
+def decode(morse: Morse) -> str:
+    """Decoding Morse code to text"""
+    text = ""
+
+    for code in morse:
+        if code in morse_to_letter:
+            text = text + morse_to_letter[code]
+        else:
+            raise ValueError(f"Unknown Morse code: {code}")
+
+    return text
